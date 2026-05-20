0845d18bd1735bf3ebf07e34892811555b3cd609
Carolina Betancourt <carolinajuarezbetancourt@gmail.com>
2026-05-19 19:03:56 -0600

    cleanup: remove temp scripts

--- check-logo.js
+++
@@ -1,7 +0,0 @@
-const fs=require('child_process').execSync;
-// Verificar si el archivo fisico existe
-if(require('fs').existsSync('public/logo-cb.jpeg')){
-  console.log('logo-cb.jpeg existe en disco, solo falta re-agregarlo a Git');
-}else{
-  console.log('logo-cb.jpeg NO existe en disco!');
-}
