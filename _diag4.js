744cf5c7b7536607ba1fa01212cfbc4c7bc27596
Carolina Betancourt <carolinajuarezbetancourt@gmail.com>
2026-05-31 22:35:03 -0600

    fix: duplicado ctaDesc calculadora

--- _diag4.js
+++ _diag4.js
@@ -1,17 +1,17 @@
-var fs = require('fs');
-var c = fs.readFileSync('src/components/herramientas-gratis.tsx', 'utf8');
-var lines = c.split('\n');
-[2146,2147,2148,2149,2150,2151,2152,2153,2154,2155,2156,2157,2158,2159,2166,2167,2168,2169].forEach(function(n) {
-  if (n <= lines.length) {
-    console.log(n + ': ' + lines[n-1]);
+const fs = require('fs');
+const fp = 'src/components/herramientas-gratis.tsx';
+let h = fs.readFileSync(fp, 'utf8');
+
+const markers = ['id="btnWA"', 'id="btnWhatsapp"'];
+for (const m of markers) {
+  let idx = 0;
+  let n = 0;
+  while ((idx = h.indexOf(m, idx)) !== -1) {
+    n++;
+    const end = Math.min(h.length, idx + 800);
+    console.log('\n========== ' + m + ' #' + n + ' ==========');
+    console.log(h.slice(idx, end));
+    console.log('========== FIN ==========');
+    idx += m.length;
   }
-});
-// Also find body{ in HTML templates
-var idx = -1;
-while ((idx = c.indexOf('body{', idx + 1)) !== -1) {
-  console.log('\nbody{ at pos ' + idx + ': ' + c.substring(idx, idx + 100));
-}
-idx = -1;
-while ((idx = c.indexOf('body {', idx + 1)) !== -1) {
-  console.log('\nbody { at pos ' + idx + ': ' + c.substring(idx, idx + 100));
 }
